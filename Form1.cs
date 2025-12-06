130caa4b4072930e07441bd46436f8699e7dc2ed
Aleksandr Stepanov <visp.asap@gmail.com>
2025-12-06 14:13:20 +0600

    add comments to Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,10 @@
+/*
+ *    Экспертные системы и ИИ
+ *    Лабораторная работа #3
+ *    Распознавание текста с помощью Tesseract и OpenCV
+ *    Выполнил: ст. гр. ИВТ-223 Степанов Александр
+ */
+
 using OpenCvSharp;
 using Tesseract;
 
@@ -18,15 +25,18 @@ namespace OpticalCharacterRecognition
         {
             InitializeComponent();
 
+            // Инициализация комбобокса, который отвечает за язык изображения
             cmbLanguage.Items.Add("rus");
             cmbLanguage.Items.Add("eng");
             cmbLanguage.SelectedItem = "rus";
 
+            // Установка языка изображения по умолчанию
             imageLanguage = "rus";
         }
 
         private void buttonLoad_Click(object sender, EventArgs e)
         {
+            // Настройка диалогового окна для открытия файла
             openFileDialog1.InitialDirectory = "C:\\";
             openFileDialog1.Filter = "" +
                 "JPG files (*.jpg)|*.jpg|" +
@@ -34,6 +44,7 @@ namespace OpticalCharacterRecognition
                 "PNG files (*.png)|*.png|" +
                 "All files (*.*)|*.*";
 
+            // Открытие диалогового окна и запись пути до изображения
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 imagePath = openFileDialog1.FileName;
@@ -45,13 +56,16 @@ namespace OpticalCharacterRecognition
                 return;
             }
 
+            // Вывод выбранного изображения в левом Picture Box
             selectedImage = Image.FromFile(imagePath);
             pictureBox1.Image = selectedImage;
 
+            // Подготовка изображения к передаче в Tesseract и вывод обработанного изображения в правый PictureBox
             processedImage = ProcessImage(imagePath);
             pictureBox2.Image = processedImage;
         }
 
+        // Обработка изображения
         private Image ProcessImage(string imagePath)
         {
             Mat image = Cv2.ImRead(imagePath, ImreadModes.Color);
@@ -67,11 +81,15 @@ namespace OpticalCharacterRecognition
                 return null;
             }
 
+            // Масштабирование изображения, смена цветовой схемы на оттенки серого и бинаризация изображения методом Оцу
             Cv2.Resize(image, scaledImage, new OpenCvSharp.Size(), 3, 3, InterpolationFlags.Cubic);
             Cv2.CvtColor(image, grayImage, ColorConversionCodes.BGR2GRAY);
             Cv2.Threshold(grayImage, binaryImage, 0, 255, ThresholdTypes.Otsu);
+
+            // Запись изображения во временный файл, чтобы избежать его блокировки при удалении
             Cv2.ImWrite(tempImagePath, binaryImage);
 
+            //Получение обработанного изображения с помощью MemoryStream и его возвращение
             using MemoryStream memoryStream = new MemoryStream();
             {
                 Cv2.ImEncode(".png", binaryImage, out byte[] png);
@@ -86,8 +104,15 @@ namespace OpticalCharacterRecognition
             readTextFromImage();
         }
 
+        // Распознавание текста
         private void readTextFromImage()
         {
+            /*
+             * Инициализация движка Tesseract с помощью указания пути до языковых данных (*.traineddata), языка изображения и режима работы
+             * Ранее сохранённое обработанное изображение загружается в объект класса Pix - внутренний формат файла Tesseract
+             * Результат распознавания загружается в объект класса Page, а из него распознанный текст записывается в строку recognizedText
+             * Использую using, чтобы гарантированно освободить все используемые ресурсы при завершении работы для корректной работы последующих распознаваний
+             */
             using TesseractEngine tesseractEngine = new TesseractEngine(tesseractDataPath, imageLanguage, EngineMode.Default);
             using Pix pix = Pix.LoadFromFile(tempImagePath);
             using Page page = tesseractEngine.Process(pix);
@@ -95,11 +120,14 @@ namespace OpticalCharacterRecognition
                 recognizedText = page.GetText().Trim();
             }
 
+            // Вывод в нижний RichTextBox распознанного текста
             richTextBox1.Text = recognizedText;
 
+            // Удаление временного файла, т.е. обработанного изображения
             File.Delete(tempImagePath);
         }
 
+        // Смена языка изображения
         private void cmbLanguage_SelectedIndexChanged(object sender, EventArgs e)
         {
             switch (cmbLanguage.SelectedIndex)
--- obj/Debug/net9.0-windows/OpticalCharacterRecognition.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows/OpticalCharacterRecognition.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OpticalCharacterRecognition")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+776eb3b3591313cbb13035aa8691dbf68cbe2c5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("OpticalCharacterRecognition")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OpticalCharacterRecognition")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
